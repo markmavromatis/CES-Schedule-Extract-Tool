6d0bae3583a12efd0e6714eb35956dcd95e99733
Mark Mavromatis <markmavromatis@mac.myfiosgateway.com>
2025-12-20 14:21:03 -0500

    Code cleanup - replaced hard-coded strings with constants

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const https = require("https");
 const cheerio = require("cheerio");
 const fs = require("node:fs");
 
+const SCHEDULE_ITEM_LABEL = "ScheduleItemLabel"
+const SCHEDULE_URL_ELEMENT = "a";
+const SCHEDULE_TIME_CLASS = "p.f-heading-4";
+const SCHEDULE_TITLE_ATTRIBUTE = "aria-label";
+const SCHEDULE_DESCRIPTION_CLASS = "p.f-body-2";
+const SCHEDULE_TAGS_DIV_CLASS = "div.items-center";
+const SCHEDULE_TAGS_LABEL_CLASS = "data-topic-label";
+const LOCATION_CLASS = "p.f-caption-2";
+
 const dates = ["2026-01-06", "2026-01-07", "2026-01-08", "2026-01-09"];
 const BASE_URL = "https://www.ces.tech/schedule/?date=";
+
+// CSV Report
+const COL_HEADERS = ["Start Time", "End Time", "Title", "Description"];
 
 function httpGet(url) {
   return new Promise((resolve, reject) => {
@@ -34,7 +46,7 @@ async function scrape(aDate) {
   const $ = cheerio.load(html);
 
   const rows = [];
-  rows.push(["Start Time", "End Time", "Title", "Description"].join(","));
+  rows.push(COL_HEADERS.join(","));
 
   const timeRegex = /(\d{1,2}:\d{2}\s*[AP]M)\s*-\s*(\d{1,2}:\d{2}\s*[AP]M)/i;
 
@@ -43,22 +55,22 @@ async function scrape(aDate) {
     .find("div")
     .each(async (_, el) => {
       const elementId = el.attribs.id;
-      if (elementId && elementId.startsWith("ScheduleItemLabel")) {
+      if (elementId && elementId.startsWith(SCHEDULE_ITEM_LABEL)) {
         const timeComponents = $(el)
-          .find("p.f-heading-4")[0]
+          .find(SCHEDULE_TIME_CLASS)[0]
           .children[0].data.split("-");
         const startTime = timeComponents[0];
         const endTime = timeComponents[1];
-        const location = $(el).find("p.f-caption-2")[0].children[0].data;
-        const urlLink = $(el).find("a")[0];
+        const location = $(el).find(LOCATION_CLASS)[0].children[0].data;
+        const urlLink = $(el).find(SCHEDULE_URL_ELEMENT)[0];
         const url = urlLink.attribs.href;
-        const title = urlLink.attribs["aria-label"];
-        const description = $(el).find("p.f-body-2")[0].children[0].data;
-        const tagsElements = $(el).find("div.items-center").find("a");
+        const title = urlLink.attribs[SCHEDULE_TITLE_ATTRIBUTE];
+        const description = $(el).find(SCHEDULE_DESCRIPTION_CLASS)[0].children[0].data;
+        const tagsElements = $(el).find(SCHEDULE_TAGS_DIV_CLASS).find("a");
         let tags = "";
         for (i = 0; i < tagsElements.length; i++) {
           const tagElement = tagsElements[i];
-          tags += tagElement.attribs["data-topic-label"] + ",";
+          tags += tagElement.attribs[SCHEDULE_TAGS_LABEL_CLASS] + ",";
         }
         // tags += '"';
 
