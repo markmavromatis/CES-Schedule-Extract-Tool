eb22bf3bbb47f8fe2c9f29fe7cac32043e290a69
Mark Mavromatis <markmavromatis@gmail.com>
2025-12-21 18:48:22 -0500

    #2 Add filter tags so sessions can be filtered by tags

--- index.js
+++ index.js
@@ -12,11 +12,85 @@ const SCHEDULE_TAGS_DIV_CLASS = "div.items-center";
 const SCHEDULE_TAGS_LABEL_CLASS = "data-topic-label";
 const LOCATION_CLASS = "p.f-caption-2";
 
+const MASTER_TAGS = [
+  "Accessibility",
+  "AgTech",
+  "Artificial Intelligence",
+  "Audio",
+  "Beauty Tech",
+  "BioTech",
+  "Computing",
+  "Construction & Industrial Tech",
+  "Content & Entertainment",
+  "Creators",
+  "Cybersecurity",
+  "Digital Health",
+  "Drones",
+  "Education Tech",
+  "Energy Transition",
+  "Enterprise",
+  "Fashion Tech",
+  "Fintech",
+  "Food Tech",
+  "Future of Work",
+  "Gaming & Esports",
+  "Home Entertainment & Office Hardware",
+  "Innovation for All",
+  "Investment & Venture Capital",
+  "IoT/Sensors",
+  "Longevity",
+  "Marketing & Advertising",
+  "Next G",
+  "Policy",
+  "Quantum",
+  "Research",
+  "Resiliency",
+  "Retail/E-Commerce",
+  "Robotics",
+  "Smart Communities",
+  "Smart Home & Appliances",
+  "Sourcing & Manufacturing",
+  "Space Tech",
+  "Sports",
+  "Standards",
+  "Startups",
+  "Streaming",
+  "Sustainability",
+  "Vehicle Tech & Advanced Mobility",
+  "Wearables",
+  "Women's Health Tech",
+  "XR & Spatial Computing",
+];
+
 const dates = ["2026-01-06", "2026-01-07", "2026-01-08", "2026-01-09"];
 const BASE_URL = "https://www.ces.tech/schedule/?date=";
 
 // CSV Report
-const COL_HEADERS = ["Start Time", "End Time", "Title", "Description"];
+const COL_HEADERS = [
+  "Date",
+  "Start Time",
+  "End Time",
+  "Location",
+  "URL",
+  "Tags",
+  "Title",
+  "Description",
+];
+
+let tagFilters = [];
+
+// Process tag filter arguments and throw exception for unknown tags
+function loadTagFilters() {
+  const argsLength = process.argv.length;
+  for (let i = 2; i < argsLength; i++) {
+    const track = process.argv[i];
+    if (!MASTER_TAGS.includes(track)) {
+      throw "Track " + track + "not found!";
+    } else {
+      tagFilters.push(track);
+    }
+  }
+}
 
 function httpGet(url) {
   return new Promise((resolve, reject) => {
@@ -49,6 +123,7 @@ async function scrape(aDate) {
 
   // Find results
   $("div[id^='" + SCHEDULE_ITEM_LABEL + "']").each((_, el) => {
+    let includeRow = false;
     const timeComponents = $(el)
       .find(SCHEDULE_TIME_CLASS)[0]
       .children[0].data.split("-");
@@ -62,24 +137,34 @@ async function scrape(aDate) {
       .data;
     const tagsElements = $(el).find(SCHEDULE_TAGS_DIV_CLASS).find("a");
     let tags = "";
-    for (i = 0; i < tagsElements.length; i++) {
-      const tagElement = tagsElements[i];
-      tags += tagElement.attribs[SCHEDULE_TAGS_LABEL_CLASS] + ",";
+    if (tagFilters.length == 0) {
+      includeRow = true;
+    } else {
+      for (i = 0; i < tagsElements.length; i++) {
+        const tagElement = tagsElements[i];
+        const tagValue = tagElement.attribs[SCHEDULE_TAGS_LABEL_CLASS];
+        if (tagFilters.includes(tagValue)) {
+          includeRow = true;
+        }
+        tags += tagValue + ",";
+      }
     }
-
-    rows.push(
-      [
-        csvEscape(aDate),
-        csvEscape(startTime),
-        csvEscape(endTime),
-        csvEscape(location),
-        csvEscape(url),
-        csvEscape(title),
-        csvEscape(description),
-      ].join(",")
-    );
-    // }
+    if (includeRow) {
+      rows.push(
+        [
+          csvEscape(aDate),
+          csvEscape(startTime),
+          csvEscape(endTime),
+          csvEscape(location),
+          csvEscape(url),
+          csvEscape(tags),
+          csvEscape(title),
+          csvEscape(description),
+        ].join(",")
+      );
+    }
   });
+
   return rows.join("\n");
 }
 
@@ -95,6 +180,7 @@ async function writeToFile(results) {
 
 async function main() {
   let results = [];
+  loadTagFilters();
   for (const aDate of dates) {
     results += await scrape(aDate).catch((err) => {
       console.error(err);
